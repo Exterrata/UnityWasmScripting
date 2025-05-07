530372d805729605be1bf0959bb1d39bb8f4becd
Exterrata <38149279+Exterrata@users.noreply.github.com>
2025-05-07 19:40:48 -0400

    changes

--- ApplyFormatting.bat
+++ ApplyFormatting.bat
@@ -1,11 +1,5 @@
 setlocal enabledelayedexpansion
 
-if "%1"=="smudge" (
-    copy /Y ".local.editorconfig" ".editorconfig" >nul
-) else (
-    copy /Y ".repo.editorconfig"  ".editorconfig" >nul
-)
-
 set PROJECTS= "WasmScripting.csproj" "WasmScripting.Editor.csproj" "WasmScripting.Links.csproj" "./Assets/.WasmModule/WasmModule.csproj"
 
 for %%P in (%PROJECTS%) do (
